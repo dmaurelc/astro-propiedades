faaad7e7855789d558541793938595640536ad4f
DanielMC <dmaurelc@gmail.com>
2024-06-17 17:47:57 -0400

    Update config tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,10 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/@wp-block-tools/styles/**/*.js"],
 	theme: {
+		fontFamily: {
+			body: ["Outfit", "Arial", "sans-serif"],
+			heading: ["Outfit", "Arial", "sans-serif"],
+		},
 		extend: {
 			aspectRatio: {
 				'landscape': '4 / 3',
