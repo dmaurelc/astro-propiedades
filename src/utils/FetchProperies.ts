144217fb4f2466120bc35332b0644e52ee0686aa
DanielMC <dmaurelc@gmail.com>
2024-06-21 15:02:34 -0400

    New custom endpoint and update home page properties

--- src/utils/FetchProperies.ts
+++ src/utils/FetchProperies.ts
@@ -1,5 +1,18 @@
 export async function fetchProperties() {
-  const response = await fetch('https://www.hugoortizpropiedades.cl/wp-json/custom/v1/propiedades/');
+  const response = await fetch(
+    "https://www.hugoortizpropiedades.cl/wp-json/custom/v1/propiedades/",
+  );
   const propiedades = await response.json();
   return propiedades;
-}  
+}
+
+export async function fetchDestacadas() {
+  const response = await fetch(
+    "https://www.hugoortizpropiedades.cl/wp-json/custom/v1/propiedades/destacadas",
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch properties");
+  }
+  const data = await response.json();
+  return data;
+}
